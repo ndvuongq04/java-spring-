4d8e2083336091ce752cb2261b4fcc2b473929ba
ndvuongq04 <nguyendinhvuong08122004@gmail.com>
2024-10-09 21:54:17 +0700

    test vd 17

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with Nguyễn Đình Vượng ";
+        return "Hello World with Vượng học code mỗi ngày ";
     }
 }
